e51e7c80b3d82a250cddf1ceb5c051b454044f54
Rafal Jedel <noahh.r@gmail.com>
2020-10-16 23:46:11 +0200

    modify view

--- reservation/urls.py
+++ reservation/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('room/new/', views.room_new_view),
     path('rooms/', views.rooms_view),
     path('room/delete/<int:id>/', views.room_delete_by_id_view),
+    path('room/modify/<int:id>/', views.room_modify_by_id_view),
 ]
--- reservation/views.py
+++ reservation/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render, redirect
 
 from .models import Room
@@ -30,7 +30,7 @@ def room_new_view(request):
             return render(request, 'reservation/room_new.html', context={'message': message})
         else:
             Room.objects.create(name=name, capacity=int(capacity), projector=projector)
-            return redirect('/')
+            return redirect('/rooms/')
 
 
 def rooms_view(request):
@@ -41,7 +41,44 @@ def room_delete_by_id_view(request, id):
     try:
         room = Room.objects.get(pk=id)
     except Room.DoesNotExist:
-        return HttpResponse('no room id {}'.format(id))
+        raise Http404
     else:
         room.delete()
         return redirect('/rooms/')
+
+
+def room_modify_by_id_view(request, id):
+    try:
+        room = Room.objects.get(pk=id)
+    except Room.DoesNotExist:
+        raise Http404
+    else:
+        if request.method == 'GET':
+            return render(request, 'reservation/room_modify.html', context={'room': room})
+        if request.method == 'POST':
+            name = request.POST.get('name')
+            capacity = request.POST.get('capacity')
+            projector = request.POST.get('projector')
+            message = ''
+            if name is None or len(name) < 1:
+                message = 'enter room name'
+            # if name in [room.name for room in Room.objects.all()]:
+            #     message = 'this room already exists'
+            try:
+                if int(capacity) < 1:
+                    message = 'capacity must be greater than 1'
+            except (ValueError, TypeError):
+                message = 'enter an integer'
+            if projector == "True":
+                projector = True
+            else:
+                projector = False
+
+            if message:
+                return render(request, 'reservation/room_modify.html', context={'message': message})
+            else:
+                room.name = name
+                room.capacity = int(capacity)
+                room.projector = projector
+                room.save()
+                return redirect('/rooms/')
